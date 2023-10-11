148ae560fca5470498bcabdf085361f4fba65581
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-10-11 08:54:45 +0200

    Added an .env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './App.css'
 import Newsletter from './components/Newsletter/newsletter'
-import Modal from './components/Modal/modal'
 
 function App() {
 
   return (
     <>
       <Newsletter/>
-      <Modal />
     </>
   )
 }
